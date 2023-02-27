582180c6c09da64a839bff5d313f6210f6c1150f
igoradulyan <i.adulian@gmail.com>
2023-02-27 10:24:43 -0800

    Validation Fixed

--- src/main/java/com/example/demo/Validation/FieldMatch.java
+++ src/main/java/com/example/demo/Validation/FieldMatch.java
@@ -6,24 +6,22 @@ import jakarta.validation.Constraint;
 import java.lang.annotation.Documented;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.TYPE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.lang.annotation.*;
 
-@Target({TYPE, ANNOTATION_TYPE})
-@Retention(RUNTIME)
 @Constraint(validatedBy = FieldMatchValidator.class)
+@Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+@Retention(RetentionPolicy.RUNTIME)
 @Documented
-public @interface FieldMatch
-{
-    String message() default "{constraints.field-match}";
+public @interface FieldMatch {
+    String message() default "";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
+
     String first();
     String second();
 
-    @Target({TYPE, ANNOTATION_TYPE})
-    @Retention(RUNTIME)
+    @Target({ ElementType.TYPE, ElementType.ANNOTATION_TYPE })
+    @Retention(RetentionPolicy.RUNTIME)
     @Documented
     @interface List
     {

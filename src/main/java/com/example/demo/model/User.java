a713aa42f52095037d87873c559655ec2066b54b
haoranli <lihaoran_0826@outlook.com>
2023-02-27 07:55:57 -0800

    feat(main):init project of upload-to-github

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,28 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Collection;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@EqualsAndHashCode
+@ToString
+public class User {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private  int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinTable(name = "user_movies", joinColumns = @JoinColumn (name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name ="movie_id"))
+    private Collection<Movie> movies;
+}
